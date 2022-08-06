1a3edf9d631d62442c1121e824f84f81daa6aafd
Touhid Md Abdullah Al <66207928+appletouhid@users.noreply.github.com>
2022-08-06 20:05:18 +0600

    Add files via upload

---
+++ Code/ClientThread.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienttcp;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author abaltouhid
+ */
+public class ClientThread extends Thread {
+
+    private Socket socket = null;
+    private ClientTCP client = null;
+    private DataInputStream streamIn = null;
+
+    public ClientThread(ClientTCP _client, Socket _socket) {
+        client = _client;
+        socket = _socket;
+        open();
+        start();
+    }
+
+    public void open() {
+        try {
+            streamIn = new DataInputStream(socket.getInputStream());
+        } catch (IOException ioe) {
+            System.out.println("Error getting input stream: " + ioe);
+            client.stop();
+        }
+    }
+
+    public void close() {
+        try {
+            if (streamIn != null) {
+                streamIn.close();
+            }
+        } catch (IOException ioe) {
+            System.out.println("Error closing input stream: " + ioe);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                client.handle(streamIn.readUTF());
+            } catch (IOException ioe) {
+                System.out.println("Listening error: " + ioe.getMessage());
+                client.stop();
+            }
+        }
+    }
+}
